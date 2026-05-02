d08c53770e83a6231f53c561b790eacc2e89d4d2
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:19:16 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Insert Source Code
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/insertSourceCode?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-other-lightgrey.svg)
+
+Insert the source code directly into the blocks, so that professional users can realize some more advanced functions.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,87 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'insertSourceCode.name': 'Insert Source Code',
+            'insertSourceCode.description': 'Insert the source code directly into the blocks, so that professional users can realize some more advanced functions.'
+        },
+        'ru': {
+            'insertSourceCode.name': 'Insert Source Code',
+            'insertSourceCode.description': 'Insert the source code directly into the blocks, so that professional users can realize some more advanced functions.'
+        },
+        'zh-cn': {
+            'insertSourceCode.name': 'Insert Source Code',
+            'insertSourceCode.description': 'Insert the source code directly into the blocks, so that professional users can realize some more advanced functions.'
+        },
+        'zh-tw': {
+            'insertSourceCode.name': 'Insert Source Code',
+            'insertSourceCode.description': 'Insert the source code directly into the blocks, so that professional users can realize some more advanced functions.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'INSERTSOURCECODE_CATEGORY': 'Insert Source Code',
+            'INSERTSOURCECODE_COMMANDBLOCK': 'insert source code %1 %2',
+            'INSERTSOURCECODE_ESCAPE': 'escape newline',
+            'INSERTSOURCECODE_DONTESCAPE': 'don"t escape newline',
+            'INSERTSOURCECODE_ROUNDBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_BOOLEANBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_INSERTININCLUDEAREA': 'insert source code %1 in include area %2',
+            'INSERTSOURCECODE_INSERTINDEFINEAREA': 'insert source code %1 in define area %2'
+        },
+        'ru': {
+            'INSERTSOURCECODE_CATEGORY': 'Insert Source Code',
+            'INSERTSOURCECODE_COMMANDBLOCK': 'insert source code %1 %2',
+            'INSERTSOURCECODE_ESCAPE': 'escape newline',
+            'INSERTSOURCECODE_DONTESCAPE': 'don"t escape newline',
+            'INSERTSOURCECODE_ROUNDBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_BOOLEANBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_INSERTININCLUDEAREA': 'insert source code %1 in include area %2',
+            'INSERTSOURCECODE_INSERTINDEFINEAREA': 'insert source code %1 in define area %2'
+        },
+        'zh-cn': {
+            'INSERTSOURCECODE_CATEGORY': 'Insert Source Code',
+            'INSERTSOURCECODE_COMMANDBLOCK': 'insert source code %1 %2',
+            'INSERTSOURCECODE_ESCAPE': 'escape newline',
+            'INSERTSOURCECODE_DONTESCAPE': 'don"t escape newline',
+            'INSERTSOURCECODE_ROUNDBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_BOOLEANBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_INSERTININCLUDEAREA': 'insert source code %1 in include area %2',
+            'INSERTSOURCECODE_INSERTINDEFINEAREA': 'insert source code %1 in define area %2'
+        },
+        'zh-tw': {
+            'INSERTSOURCECODE_CATEGORY': 'Insert Source Code',
+            'INSERTSOURCECODE_COMMANDBLOCK': 'insert source code %1 %2',
+            'INSERTSOURCECODE_ESCAPE': 'escape newline',
+            'INSERTSOURCECODE_DONTESCAPE': 'don"t escape newline',
+            'INSERTSOURCECODE_ROUNDBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_BOOLEANBLOCK': 'insert source code %1',
+            'INSERTSOURCECODE_INSERTININCLUDEAREA': 'insert source code %1 in include area %2',
+            'INSERTSOURCECODE_INSERTINDEFINEAREA': 'insert source code %1 in define area %2'
+        }
+    }
+};
